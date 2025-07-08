885efc3507dd5a3de55eb5787046d3fdd0e9b094
Raman <khanahmadi.raman@gmail.com>
2025-07-08 18:51:13 +0300

    complete Services

--- src/app/Services/task.service.ts
+++ src/app/Services/task.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../Model/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Learning Angular',
+      description: 'Completion of the test project',
+      status: 'pending',
+    },
+  ];
+  private nextId = 2;
 
-  constructor() { }
+  getTask(id: number): Task | undefined {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  addTask(title: string, description: string): void {
+    const newTask: Task = {
+      id: this.nextId++,
+      title,
+      description,
+      status: 'pending',
+    };
+    this.tasks.push(newTask);
+  }
+
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
