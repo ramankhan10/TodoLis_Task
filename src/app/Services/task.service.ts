13bb95846da0ea2ccd17e0bb0025c14c3a213ac1
Raman <khanahmadi.raman@gmail.com>
2025-07-09 10:51:27 +0300

    add localStorage

--- src/app/Services/task.service.ts
+++ src/app/Services/task.service.ts
@@ -5,25 +5,46 @@ import { Task } from '../Model/task';
   providedIn: 'root',
 })
 export class TaskService {
-  
-  private tasks: Task[] = [
-    {
-      id: 1,
-      title: 'Learning Angular',
-      description: 'Completion of the test project',
-      status: 'pending',
-    },
-  ];
-  private nextId = 2;
+  private tasks: Task[] = [];
+  private storageKey = 'my-app-tasks';
+  private nextId = 1;
 
-  addTask(title: string, description: string): void {
-    const newTask: Task = {
-      id: this.nextId++,
-      title,
-      description,
-      status: 'pending',
-    };
-    this.tasks.push(newTask);
+  constructor() {
+    this.loadTasksStorage();
+  }
+
+  private saveToStorage(): void {
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem(this.storageKey, JSON.stringify(this.tasks));
+    }
+  }
+
+  private loadTasksStorage(): void {
+    if (typeof localStorage !== 'undefined') {
+      const savedTasks = localStorage.getItem(this.storageKey);
+
+      if (savedTasks) {
+        this.tasks = JSON.parse(savedTasks);
+      } else {
+        this.tasks = [
+          {
+            id: 1,
+            title: 'Learning Angular',
+            description: 'Completion of the test project',
+            status: 'pending',
+          },
+        ];
+      }
+    }
+    this.updateNextId();
+  }
+
+  private updateNextId(): void {
+    if (this.tasks.length > 0) {
+      this.nextId = Math.max(...this.tasks.map((task) => task.id)) + 1;
+    } else {
+      this.nextId = 1;
+    }
   }
 
   getTasks(): Task[] {
@@ -33,7 +54,22 @@ export class TaskService {
   getTask(id: number): Task | undefined {
     return this.tasks.find((task) => task.id === id);
   }
+
+  addTask(title: string, description: string): void {
+    const newTask: Task = {
+      id: this.nextId,
+      title,
+      description,
+      status: 'pending',
+    };
+    this.tasks.push(newTask);
+    this.saveToStorage();
+    this.updateNextId();
+  }
+
   deleteTask(id: number): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveToStorage();
+    this.updateNextId();
   }
 }
